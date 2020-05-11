9e94c92d1e317055b7145fab404315a53fcf9843
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-05-11 11:25:48 +0100

    Working POC downwards scroll both ways

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import Section from './components/Section';
 import GenerateHexColor from './utils/GenerateHexColor';
-import './App.css';
 
 class App extends React.Component {
   constructor() {
     super(...arguments);
     this.state = {
+      prevSections: [],
       sections: [
-        <Section key={0} backgroundColor={GenerateHexColor()} />,
-        <Section key={1} backgroundColor={GenerateHexColor()} />,
-        <Section key={2} backgroundColor={GenerateHexColor()} />
+        <Section key={0} index={0} bgColor={GenerateHexColor()} />,
+        <Section key={1} index={1} bgColor={GenerateHexColor()} />,
+        <Section key={2} index={2} bgColor={GenerateHexColor()} />
       ]
     }
   }
 
   componentDidMount() {
+    let lastScroll = 0;
     window.addEventListener('scroll', () => {
+      const scrollingUp = window.pageYOffset < lastScroll;
+
       if (window.pageYOffset > window.innerHeight) {
         const newKey = Number(this.state.sections[2].key) + 1;
-        this.state.sections.shift();
+        const prevSection = this.state.sections.shift();
+        this.setState({ prevSections: [...this.state.prevSections, prevSection] });
         this.setState({
-          sections: [...this.state.sections, <Section key={newKey} backgroundColor={GenerateHexColor()} />]
+          sections: [
+            ...this.state.sections,
+            <Section key={newKey} index={newKey} bgColor={GenerateHexColor()} />
+          ]
         });
       }
+
+      if (scrollingUp && window.pageYOffset === 0 && this.state.prevSections.length) {
+        this.state.sections.pop();
+        this.setState({
+          sections: [ this.state.prevSections.pop(), ...this.state.sections ]
+        });
+        window.scrollTo(0, window.innerHeight)
+      }
+      lastScroll = window.pageYOffset;
     });
   }
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -4,8 +4,9 @@ import './Section.css';
 class Section extends React.Component {
   render() {
     return (
-      <section style={{backgroundColor: this.props.backgroundColor}}>
-        <h1>Background color: {this.props.backgroundColor}</h1>
+      <section style={{backgroundColor: this.props.bgColor}} id={this.props.index}>
+        <h1>{this.props.index}</h1>
+        <small>Background color: <code>{this.props.bgColor}</code></small>
       </section>
     )
   }
