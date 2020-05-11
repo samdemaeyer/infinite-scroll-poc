b355cb824c88a7e3e61990a916fff8e1e79db0e3
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-05-11 11:27:54 +0100

    Updates

---
+++ precache-manifest.bea1350cc469273d36f0d3685b8c3713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9514b72a2fd66291d41a0f9504f513",
+    "url": "/infinite-scroll-poc/index.html"
+  },
+  {
+    "revision": "459c9e2beee97ce2d741",
+    "url": "/infinite-scroll-poc/static/css/main.7d0915c6.chunk.css"
+  },
+  {
+    "revision": "cd20141c6a5017c69fe7",
+    "url": "/infinite-scroll-poc/static/js/2.e36c9612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinite-scroll-poc/static/js/2.e36c9612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459c9e2beee97ce2d741",
+    "url": "/infinite-scroll-poc/static/js/main.86abac0f.chunk.js"
+  },
+  {
+    "revision": "67dd058681af48258d34",
+    "url": "/infinite-scroll-poc/static/js/runtime-main.5ecc42e1.js"
+  }
+]);
